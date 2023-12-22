a2fdb66e2acd7dcc864fa1e481d1b24daebca45c
Chaxo <chaxo.he@gmail.com>
2023-12-22 01:13:22 +0100

    streamlit start

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import requests
-import json
+import streamlit as st
 
 if __name__ == '__main__':
+    location = st.text_input('Enter your location')
     api_key = "53e70973ea12442ea8f205919231912"
-    location = "est netherlands"
-    url = "http://api.weatherapi.com/v1/forecast.json"
-    params = {
-        'key' : api_key,
-        'q' : location,
-        'days' : 1,
-        'aqi' : 'no',
-        'alerts': 'no'
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    r = requests.get(url=url, params=params, headers=headers)
-    response = r.json()
-    place = response['location']['name']
-    region = response['location']['region']
-    country = response['location']['country']
-    # print(response['forecast']['forecastday'][0]['day'])
-    print(response)
+    if location:
+        url = "http://api.weatherapi.com/v1/forecast.json"
+        params = {
+            'key' : api_key,
+            'q' : location,
+            'days' : 1,
+            'aqi' : 'no',
+            'alerts': 'no'
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        r = requests.get(url=url, params=params, headers=headers)
+        response = r.json()
+        place = response['location']['name']
+        region = response['location']['region']
+        country = response['location']['country']
+        # print(response['forecast']['forecastday'][0]['day'])
+        st.write(response)
